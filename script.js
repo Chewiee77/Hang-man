bcab1a603d9d6e3a5aa5bc6632dd46387385b4b4
Chewiee77 <chewiee77@gmail.com>
2023-02-02 22:07:06 +0100

    Some minor fixes before bedtime

--- script.js
+++ script.js
@@ -2,24 +2,30 @@ import words from "/svenska-ord.json" assert { type: "json" };
 
 const scoreBoardBtn = document.querySelector(".scoreboard");
 const pvpBtn = document.querySelector(".pvp");
-
 let letterBoxes;
-let startGameBtn = document.querySelector("#startGameBtn");
+let startGameBtn;
+let selectedWord;
 
 const improvedWordList = words.filter((word) => !word.includes(" "));
 console.log(improvedWordList);
 const finalWordList = improvedWordList.filter((word) => !word.includes("-"));
 console.log(finalWordList);
-let selectedWord = words[Math.floor(Math.random() * words.length)];
+// let selectedWord = finalWordList[Math.floor(Math.random() * words.length)];
 // console.log(words);
 // console.log(words.length);
 // console.log(selectedWord);
 // console.log(selectedWord.includes("-"));
 
+function startGame() {
+  pickAWord();
+  showEmptyLetterBoxes();
+}
+startGameBtn = document.querySelector("#startGameBtn");
 document.querySelector("#startGameBtn").addEventListener("click", startGame);
 
 function pickAWord() {
-  let selectedWord = finalWordList[Math.floor(Math.random() * words.length)];
+  selectedWord =
+    finalWordList[Math.floor(Math.random() * finalWordList.length)];
 
   // console.log(selectedWord);
 }
@@ -28,7 +34,7 @@ function pickAWord() {
 
 function showEmptyLetterBoxes() {
   let letterBox = "";
-
+  console.log(selectedWord);
   for (let i = 0; i < selectedWord.length; i++) {
     console.log(selectedWord);
     console.log(selectedWord.length);
@@ -39,9 +45,4 @@ function showEmptyLetterBoxes() {
   document.querySelector(".gamespace").innerHTML = letterBox;
 
   letterBoxes = document.querySelector(".gamespace").querySelector(".box");
-}
-
-function startGame() {
-  pickAWord();
-  showEmptyLetterBoxes();
 }
