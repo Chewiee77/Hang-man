f51ac4cf3c8705e1c93b9dc698e051f33d681a12
MiladGezabi <milad.gezabi@gmail.com>
2023-02-02 17:47:09 +0100

    added new variables

--- script.js
+++ script.js
@@ -4,5 +4,13 @@ const SCOREBOARDBTN = document.querySelector(".scoreboard");
 const PVPBTN = document.querySelector(".pvp")
 const PLAYBTN = document.querySelector("#startGameBtn")
 
+const GROUND = document.querySelector("#ground")
+const SCAFFOLD = document.querySelector("#scaffold")
+const HEAD = document.querySelector("#head")
+const BODY = document.querySelector("#body")
+const ARMS = document.querySelector("#arms")
+const LEGS = document.querySelector("#legs")
 
-let selectedWord = words[Math.floor(Math.random()* words.lenght)];
+
+let selectedWord = words[Math.floor(Math.random()* words.length)];
+
