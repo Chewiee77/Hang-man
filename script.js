d370ce11ca08fc945bb8c4f25ce9fd85697d5f8e
Chewiee77 <chewiee77@gmail.com>
2023-02-02 19:51:07 +0100

    sorting of array to exclude - and emty space. Functioning startbutton and displaying boxes on buttonpress

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
-import words from "/svenska-ord.json" assert {type: "json"};
+import words from "/svenska-ord.json" assert { type: "json" };
 
+let letterBoxes;
+let startGameBtn = document.querySelector("#startGameBtn");
 
-let selectedWord = words[Math.floor(math.random()* words.lenght)];
+const improvedWordList = words.filter((word) => !word.includes(" "));
+console.log(improvedWordList);
+const finalWordList = improvedWordList.filter((word) => !word.includes("-"));
+console.log(finalWordList);
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+// console.log(words);
+// console.log(words.length);
+// console.log(selectedWord);
+// console.log(selectedWord.includes("-"));
+
+document.querySelector("#startGameBtn").addEventListener("click", startGame);
+
+function pickAWord() {
+  let selectedWord = finalWordList[Math.floor(Math.random() * words.length)];
+
+  // console.log(selectedWord);
+}
+
+// pickAWord();
+
+function showEmptyLetterBoxes() {
+  let letterBox = "";
+
+  for (let i = 0; i < selectedWord.length; i++) {
+    console.log(selectedWord);
+    console.log(selectedWord.length);
+    letterBox += '<span class="box">&nbsp;</span>';
+    console.log(i);
+  }
+
+  document.querySelector(".gamespace").innerHTML = letterBox;
+
+  letterBoxes = document.querySelector(".gamespace").querySelector(".box");
+}
+
+function startGame() {
+  pickAWord();
+  showEmptyLetterBoxes();
+}
