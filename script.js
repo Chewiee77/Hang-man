bad44ccc2c69efecab1b0e5b2de79d0da54f1c6f
MiladGezabi <milad.gezabi@gmail.com>
2023-02-02 16:53:32 +0100

    edited js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 import words from "/svenska-ord.json" assert {type: "json"};
 
+const SCOREBOARDBTN = document.querySelector(".scoreboard");
+const PVPBTN = document.querySelector(".pvp")
+const PLAYBTN = document.querySelector("#startGameBtn")
+
 
 let selectedWord = words[Math.floor(math.random()* words.lenght)];
