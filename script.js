dd6b47c9e3475fdda5312b9610d335cf215e079a
MalinJ123 <malin--jonsson@hotmail.com>
2023-02-02 17:21:02 +0100

    någonting

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 import words from "/svenska-ord.json" assert {type: "json"};
 
-const SCOREBOARDBTN = document.querySelector(".scoreboard");
-const PVPBTN = document.querySelector(".pvp")
-const PLAYBTN = document.querySelector("#startGameBtn")
+const scoreBoardBtn = document.querySelector(".scoreboard");
+const pvpBtn = document.querySelector(".pvp")
+const playBtn = document.querySelector("#startGameBtn")
 
 
 let selectedWord = words[Math.floor(math.random()* words.lenght)];
+console.log(selectedWord)
+
