7809f84cc81856c078bfaba73d430a08a6ff1940
MalinJ123 <malin--jonsson@hotmail.com>
2023-02-05 19:02:13 +0100

    Lagt till namn i localstorage, och h1

--- script.js
+++ script.js
@@ -11,101 +11,92 @@ let selectedWord;
 
 // Sorterar bor ord som innehåller mellanslag och - 
 // Varför är inte detta en funktion?
-// const improvedWordList = words.filter((word) => !word.includes(" "));
-// console.log(improvedWordList);
-// const finalWordList = improvedWordList.filter((word) => !word.includes("-"));
+const improvedWordList = words.filter((word) => !word.includes(" "));
+console.log(improvedWordList);
+const finalWordList = improvedWordList.filter((word) => !word.includes("-"));
 
-// // let selectedWord = finalWordList[Math.floor(Math.random() * words.length)];
+// let selectedWord = finalWordList[Math.floor(Math.random() * words.length)];
 
-// console.log(finalWordList);
-// console.log(words);
-// console.log(words.length);
-// console.log(selectedWord);
-// // console.log(selectedWord.includes("-")); 
+console.log(finalWordList);
+console.log(words);
+console.log(words.length);
+console.log(selectedWord);
+// console.log(selectedWord.includes("-")); 
 
-// function startGame() {
-//       pickAWord();
-//       showEmptyLetterBoxes();
-//       // changeButtonActivation(false);
-// }
-// startGameBtn = document.querySelector("#startGameBtn");
-// document.querySelector("#startGameBtn").addEventListener("click", startGame);
-
-
-// // Genererar ett random ord i listan
-// function pickAWord() {
-//   selectedWord = 
-//     finalWordList[Math.floor(Math.random() * finalWordList.length)];
-
-//   console.log(selectedWord);
-//   // alert(selectedWord)
-// }
-// pickAWord();
-
-
-// function showEmptyLetterBoxes() {
-//   let letterBox = " "; 
-//   console.log(selectedWord);
-//   for (let i = 0; i < selectedWord.length; i ++) {
-//     console.log(selectedWord);
-//     console.log(selectedWord.length);
-//     letterBox += '<span class="box">&nbsp;</span>';
-//     console.log(i);
-//     console.log(pickAWord)
-//   }
-
-//   document.querySelector(".gamespace").innerHTML = letterBox;
-
-//   letterBoxes = document.querySelector(".gamespace").querySelector(".box");
-// }
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------Här fungerar koden 
-
-
-//  Hur Många gissningar av 6  ---------------------------------------
-let maxWrong = 6;
-document.getElementById('maxWrong').innerHTML = maxWrong
-// ---------------------------------------------------------
-
-// Kod för figuren
-
-// Nytt malin --------------------------------------
-
-// Jag vill ta emot ett namn
-// -sätta namnet i h1 taggen. 
-console.log('hello world')
- let h1 = document.querySelector('h1')
-document.querySelector('.nameH1Btn').addEventListener('click', () => {
-    console.log('Du klickade på Namnknappen')
-  })
-  
-
-// Här börjar checkboxen
-  let toggleVisibilityText = document.querySelector('#toggle-visibility-text')
-let state = {
-  isVisible: true
+function startGame() {
+      pickAWord();
+      showEmptyLetterBoxes();
+      // changeButtonActivation(false);
 }
+startGameBtn = document.querySelector("#startGameBtn");
+document.querySelector("#startGameBtn").addEventListener("click", startGame);
 
-let toggleVisibility = document.querySelector('#toggle-visibility').addEventListener('input', event =>{
-  const checked = event.target.checked
-  console.log('Checkbox change (input event) -> ', checked)
 
-  state.isVisible = checked
+// Genererar ett random ord i listan
+function pickAWord() {
+  selectedWord = 
+    finalWordList[Math.floor(Math.random() * finalWordList.length)];
 
-  if(state.isVisible){
-    toggleVisibilityText.classList.remove('invisible')
-    // när det är synligt ta bort texten/listan
+  console.log(selectedWord);
+  // alert(selectedWord)
+}
+pickAWord();
 
-  } else{
-    toggleVisibilityText.classList.add('invisible')
-    //när den är osynlig lägg till listan 
+
+function showEmptyLetterBoxes() {
+  let letterBox = " "; 
+  console.log(selectedWord);
+  for (let i = 0; i < selectedWord.length; i ++) {
+    console.log(selectedWord);
+    console.log(selectedWord.length);
+    letterBox += '<span class="box">&nbsp;</span>';
+    console.log(i);
+    console.log(pickAWord)
   }
 
-})
+  document.querySelector(".gamespace").innerHTML = letterBox;
+
+  letterBoxes = document.querySelector(".gamespace").querySelector(".box");
+}
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
 
 
 
 
-  
+// Nytt från malin --------------------------------------
+
+// - jag vill ta emot ett namn
+// - sätta namnet i h1 taggen
+// - spara namnet i local storage 
+
+//  H1 och input
+const remember = {
+  heading: document.querySelector('#remember > h1'), 
+  input: document.querySelector('#remember > input')
+}
+// Key för Localstorage
+const LS_KEY = 'hangman_Key_toLocalStorage'
+// prata med gruppen om att byta namn på localStoragekey? 
+
+// För att lägga namnet ifrån input och i H1 meningen
+remember.input.addEventListener('input', event => {
+  const value = event.target.value
+  localStorage.setItem('LS_KEY', value)
+
+  renderRememnerHeading(value)
+});
+function renderRememnerHeading(value){
+  remember.heading.innerText = `Välkommen ${value}!`
+}
+
+// När webbsidan laddas hämtas det sparade namnent ifrån den lokala databasen
+const savedName = localStorage.getItem(LS_KEY)
+if(savedName !== '' && savedName !== null ){
+  renderRememnerHeading(savedName)
+  remember.input.value = savedName
+}
+// ---------------------------------------------------------------
+// Till måndag: Vill att det händer något med input fältet efter man skrivit sitt namn (typ att den försvinner eller något)- fråga gruppen på skolan 
 
 
 
@@ -127,43 +118,3 @@ let toggleVisibility = document.querySelector('#toggle-visibility').addEventList
 
 
 
-
-
-
-//  fungerar för att klicak på knappen ---------------------
-//  let h1 = document.querySelector('h1')
-// //  console.log('h1', h1);
-
-// document.querySelector('.nameH1Btn').addEventListener('click', () => {
-//   console.log('Du klickade på Namnknappen')
-
-// })
- 
-//  console.log('hello world 3')----------------------------------------------------------------------------------------------
-
-
-
-
-// FUNGERANDE CHECKBOX - SYNLIG TEXT
-
-// let toggleVisibilityText = document.querySelector('#toggle-visibility-text')
-
-// let state = {
-//   isVisible: true
-// }
-// let toggleVisibility = document.querySelector('#toggle-visibility').addEventListener('input', event =>{
-//   const checked = event.target.checked
-//   console.log('Checkbox change (input event) -> ', checked)
-
-//   state.isVisible = checked
-
-//   if(state.isVisible){
-//     toggleVisibilityText.classList.remove('invisible')
-//     // när det är synligt ta bort texten/listan
-
-//   } else{
-//     toggleVisibilityText.classList.add('invisible')
-//     //när den är osynlig lägg till listan 
-//   }
-
-// })---------------------------
